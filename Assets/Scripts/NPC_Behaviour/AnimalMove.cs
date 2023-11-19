cfa484a2c251439c8f1eb62f13516103b64525de
siponek <szinkowicz@gmail.com>
2023-11-19 01:13:42 +0100

    feat: removed clamping movment for animalMove

--- Assets/Scripts/NPC_Behaviour/AnimalMove.cs
+++ Assets/Scripts/NPC_Behaviour/AnimalMove.cs
@@ -3,10 +3,6 @@ using UnityEngine;
 public class AnimalMove : MonoBehaviour
 {
     public float movementSpeed = 3f;
-    public float xMin = -15f;
-    public float xMax = 15f;
-    public float zMin = -15f;
-    public float zMax = 15f;
     private string animalName;
 
     void Update()
@@ -15,13 +11,6 @@ public class AnimalMove : MonoBehaviour
         Vector3 motionDirection = new Vector3(0, 0, movementSpeed);
         // Move the animal
         transform.Translate(movementSpeed * Time.deltaTime * motionDirection);
-
-        // Clamp the player's position within the bounds
-        transform.position = new Vector3(
-            transform.position.x,
-            transform.position.y,
-            Mathf.Clamp(transform.position.z, zMin, zMax)
-        );
     }
 
     void Start()
