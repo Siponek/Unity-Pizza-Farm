84744a56cb563ad18d3014349f68928c90dd899d
siponek <szinkowicz@gmail.com>
2023-11-19 01:14:07 +0100

    feat: added tooltips, ranges and random spawning

--- Assets/Scripts/AssetsManagment/SpawnManager.cs
+++ Assets/Scripts/AssetsManagment/SpawnManager.cs
@@ -1,29 +1,28 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
-/** 
- * This manages the spawns on the map and can be called to spawn enemies
+/** This manages the spawns on the map and can be called to spawn enemies
  */
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
-    public int maxEnemies = 10;
-    public float spawnRate = 1f;
-    public float spawnTimer = 0f;
+    [Tooltip("How often to spawn enemies manually when pressing a button")]
+    [Range(1f, 10f)]
+    public float manualSpawnRate = 1f;
+    [SerializeField]
+    [Tooltip("How often to spawn enemies automatically")]
+    [Range(1f, 10f)]
+    public float autoSpawnRate = 5f;
     public float spawnRadius = 10f;
     public float spawnHeight = 1f;
     public float spawnDistance = 10f;
-    public Vector3[] spawnPoints = { 
-    new Vector3(0, 0, 0),
-    new Vector3(0, 0, 0),
-    new Vector3(0, 0, 0),
-    };
     public static readonly string[] enemyTypesTags = { "cow_enemy", "doe_enemy", "dog_enemy" };
     private ObjectPool objectPoolInstance;
     private float lastSpawnTime = 0f;
 
     void Start()
     {
+        autoSpawnRate = Mathf.Clamp(autoSpawnRate, 1f, 100f);
+        manualSpawnRate = Mathf.Clamp(manualSpawnRate, 1f, 100f);
         objectPoolInstance = FindFirstObjectByType<ObjectPool>();
         if (objectPoolInstance == null)
         {
@@ -33,11 +32,15 @@ public class SpawnManager : MonoBehaviour
 
     void Update()
     {
+        if (Time.time - lastSpawnTime >= autoSpawnRate)
+        {
+            SpawnEnemy();
+            lastSpawnTime = Time.time;
+        }
     }
     public void TriggerSpawnEnemy()
     {
         SpawnEnemy();
-
     }
     private string GetRandomNameFromList(string[] enemiesArray)
     {
@@ -46,20 +49,23 @@ public class SpawnManager : MonoBehaviour
 
     private void SpawnEnemy()
     {
+
         // Break if the spawn rate is too high
-        if (Time.time - lastSpawnTime < spawnRate)
+        if (Time.time - lastSpawnTime < manualSpawnRate)
         {
             return;
         }
         lastSpawnTime = Time.time;
+
         string enemyTag = GetRandomNameFromList(enemyTypesTags);
         GameObject newEnemy = objectPoolInstance.GetObjectFromPool(poolTag: enemyTag);
         if (newEnemy != null) newEnemy.transform.position = GetRandomSpawnPoint();
-        else Debug.Log($"SpawnEnemy: Cannot spawn a new enemy. Object pool of {enemyTag} is empty");
+        else Debug.Log($"SpawnEnemy: Cannot spawn a new enemy. Object pool of avaiable {enemyTag} is empty");
     }
     private Vector3 GetRandomSpawnPoint()
     {
-        return spawnPoints[Random.Range(0, spawnPoints.Length)];
+        float randomX = Random.Range(-spawnRadius, spawnRadius);
+        float randomZ = Random.Range(-spawnHeight, spawnHeight) + spawnDistance;
+        return new Vector3(randomX, 0, randomZ);
     }
-
 }
